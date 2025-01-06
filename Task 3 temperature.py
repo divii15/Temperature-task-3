8f6a1e45980c94169f9383d15587364d6d41cae2
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:31:28 +0530

    My next task(3)(7)

--- Task 3 temperature.py
+++ Task 3 temperature.py
@@ -1,3 +1,14 @@
+#TASK 3
+#7)Get the Temparature from user.
+#if tem greater than or equal to 0 and less than 10, output will be Very cold weather.
+#if tem greater than or equal to 10 and less than 20, output will be cold weather.
+#if tem greater than or equal to 20 and less than 30, output will be Normal Weather.
+#if tem greater than or equal to 30 and less than 40, output will be Hot.
+#otherwise Very hot.
+
+
+
+
 term = int(input("Enter the temperature :"))
 if term>=0 and term< 10:
     print("Very cold weather")
